ed9b565281414352c6143e3c654c15dd7034b7d3
Neo Tanaka <neo@NeonoMacBook-Air.local>
2021-07-29 21:59:11 +0900

    fix: fixed `class` macro to parse first argument correctly. #1

--- src/classes.nim
+++ src/classes.nim
@@ -2,7 +2,6 @@ import macros
 import classespkg / [util]
 
 
-
 macro class*(name, body: untyped): untyped =
   let status = parseClassName(name)
   block:
@@ -10,11 +9,28 @@ macro class*(name, body: untyped): untyped =
       className = status.name
       baseName = status.base
       classDefStmt = block:
-        if status.isPub:
-          quote:
-            type `className`* = ref object of `baseName`
-        else:
-          quote:
-            type `className` = ref object of `baseName`
+        case status.kind
+        of Normal:
+          if status.isPub:
+            quote:
+              type `className`* = ref object of RootObj
+          else:
+            quote:
+              type `className` = ref object of RootObj
+        of Inheritance:
+          if status.isPub:
+            quote:
+              type `className`* = ref object of `baseName`
+          else:
+            quote:
+              type `className` = ref object of `baseName`
+        of Distinct:
+          if status.isPub:
+            quote:
+              type `className`* = distinct `baseName`
+          else:
+            quote:
+              type `className` = distinct `baseName`
+
 
     result = newStmtList classDefStmt
