e9274cd9722d2ed6dfa81c24182e43456af6100e
Jez Kabanoff <thesleepless@gmail.com>
2021-09-03 18:32:31 +1000

    implement class constants #56

--- src/oolib.nim
+++ src/oolib.nim
@@ -1,13 +1,13 @@
 import macros
 import oolibpkg / [util]
-
+export optBase
 
 macro class*(head, body: untyped): untyped =
   let
     status = parseHead(head)
   var
     recList = newNimNode(nnkRecList)
-    argsList, argsListWithDefault: seq[NimNode]
+    argsList, argsListWithDefault, constsList: seq[NimNode]
     cStatus: ConstructorStatus
   result = defClass(status)
   for node in body:
@@ -33,6 +33,14 @@ macro class*(head, body: untyped): untyped =
         result.add node.insertSelf(status.name)
     of nnkFuncDef, nnkIteratorDef, nnkConverterDef, nnkTemplateDef:
       result.add node.insertSelf(status.name)
+    of nnkConstSection:
+      for n in node:
+        if n[^2].isEmpty:
+          # infer type from default
+          n[^2] = newCall(ident"typeof", n[^1])
+        if n.last.isEmpty:
+          error "Consts must have a value", body
+        constsList.add n
     of nnkDiscardStmt:
       return
     else:
@@ -58,4 +66,6 @@ macro class*(head, body: untyped): untyped =
           argsListWithAsterisksRemoved
         )
     result.insertIn1st theNew
+  for c in constsList:
+    result.insertIn1st genConstant(status.name.strVal, c)
   result[0][0][2][0][2] = recList
---
+++ tests/consts.nim
@@ -0,0 +1,39 @@
+discard """
+  action: "run"
+"""
+import unittest
+import ../src/oolib
+
+class pub A {.open.}:
+  const common* = "foo"
+  const speed* = 10.0f
+
+  var pos: float32 = 0f
+
+  method update(dt: float32) {.base.} =
+    self.pos += self.speed
+
+class pub B of A:
+  const speed* = 15.0f
+
+  proc `new`(pos = 0f): B =
+    self.pos = pos
+
+var a = newA(pos = 5f)
+var b = newB(pos = 10f)
+
+check b.speed > a.speed
+
+check A.common == "foo"
+check B.common == "foo"
+
+check a.common == "foo"
+check b.common == "foo"
+
+check a.pos == 5f
+a.update(1f)
+check a.pos == 15f
+
+check b.pos == 10f
+b.update(1f)
+check b.pos == 25f
