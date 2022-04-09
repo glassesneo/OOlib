51c9d423f6ebf1deea61fe0e27e6c098591249af
Glasses-Neo <meganeo.programmer@gmail.com>
2022-04-09 18:37:35 +0900

    :recycle: Refactor

--- src/oolib/classes.nim
+++ src/oolib/classes.nim
@@ -31,22 +31,6 @@ using
   isPub: bool
 
 
-func newClassInfo(
-    isPub = false;
-    pragmas: seq[string] = @[];
-    kind = Normal;
-    name: NimNode;
-    base: NimNode = nil
-): ClassInfo =
-  (
-    isPub: isPub,
-    pragmas: pragmas,
-    kind: kind,
-    name: name,
-    base: base,
-  )
-
-
 func isDistinct(node): bool {.compileTime.} =
   node.kind == nnkCall and node[1].kind == nnkDistinctTy
 
@@ -112,114 +96,90 @@ func newResultAsgn(rhs: string): NimNode {.compileTime.} =
   newAssignment ident"result", ident rhs
 
 
-proc pickState(node; isPub): ClassInfo {.compileTime.} =
-  case node.kind
-  of nnkIdent:
-    result = newClassInfo(
-      isPub = isPub,
-      name = node
-    )
-  of nnkCall:
-    if node.isDistinct:
-      return newClassInfo(
-        isPub = isPub,
-        kind = Distinct,
-        name = node[0],
-        base = node[1][0]
-      )
-    else:
-      return newClassInfo(
-        isPub = isPub,
-        kind = Alias,
-        name = node[0],
-        base = node[1]
-      )
-    error "Unsupported syntax", node
-  of nnkInfix:
-    if node.isInheritance:
-      if node[2].kind == nnkPragmaExpr:
-        if "open" in node[2][1]:
-          warning "{.open.} is ignored in a definition of alias", node
-        return newClassInfo(
-          isPub = isPub,
-          pragmas = node[2][1].toSeq(),
-          kind = Inheritance,
-          name = node[1],
-          base = node[2][0]
-        )
-      return newClassInfo(
-        isPub = isPub,
-        kind = Inheritance,
-        name = node[1],
-        base = node[2]
-      )
-    error "Unsupported syntax", node
-  of nnkPragmaExpr:
-    # it's form `class A {.pragma.}` or `class A(T) {.pragma.}`
-    if node[0].isDistinct:
-      return newClassInfo(
-        isPub = isPub,
-        pragmas = node[1].toSeq(),
-        kind = Distinct,
-        name = node[0][0],
-        base = node[0][1][0]
-      )
-    elif node[0].kind == nnkCall:
-      if "open" in node[1]:
-        warning "{.open.} is ignored in a definition of alias", node
-      return newClassInfo(
-        isPub = isPub,
-        pragmas = node[1].toSeq(),
-        kind = Alias,
-        name = node[0][0],
-        base = node[0][1]
-      )
-    return newClassInfo(
-      isPub = isPub,
-      pragmas = node[1].toSeq(),
-      name = node[0]
-    )
-  of nnkCommand:
-    if node[1][0].eqIdent"impl":
-      return newClassInfo(
-        isPub = isPub,
-        kind = Implementation,
-        name = node[0],
-        base = node[1][1]
-      )
-    error "Unsupported syntax", node
-  else:
-    error "Unsupported syntax", node
+func inheritanceClassInfo(
+    result: var ClassInfo;
+    node: NimNode
+) {.compileTime.} =
+  if not node.isInheritance: error "Unsupported syntax", node
+  result.kind = Inheritance
+  if node[2].kind != nnkPragmaExpr:
+    # class A of B
+    result.name = node[1]
+    result.base = node[2]
+    return
+  # class A of B {.pragma.}
+  if "open" in node[2][1]:
+    warning "{.open.} is ignored in a definition of alias", node
+  result.pragmas = node[2][1].toSeq()
+  result.name = node[1]
+  result.base = node[2][0]
 
 
 proc getClassInfo*(head: NimNode): ClassInfo {.compileTime.} =
   case head.len
   of 0:
-    result = newClassInfo(name = head)
+    # class A
+    result.kind = Normal
+    result.name = head
   of 1:
     error "Unsupported syntax", head
   of 2:
-    result = pickState(
-      if head.isPub: head[1] else: head,
-      head.isPub
-    )
+    result.isPub = head.isPub
+    var node =
+      if head.isPub: head[1]
+      else: head
+    case node.kind
+    of nnkIdent:
+      # class A
+      result.kind = Normal
+      result.name = node
+    of nnkCall:
+      result.name = node[0]
+      if node.isDistinct:
+        # class A(distinct B)
+        result.kind = Distinct
+        result.base = node[1][0]
+        return
+      # class A(B)
+      result.kind = Alias
+      result.base = node[1]
+    of nnkInfix:
+      result.inheritanceClassInfo(node)
+    of nnkPragmaExpr:
+      result.pragmas = node[1].toSeq()
+      if node[0].isDistinct:
+        # class A(distinct B) {.pragma.}
+        result.kind = Distinct
+        result.name = node[0][0]
+        result.base = node[0][1][0]
+      elif node[0].kind == nnkCall:
+        # class A(B) {.pragma.}
+        if "open" in node[1]:
+          warning "{.open.} is ignored in a definition of alias", node
+        result.kind = Alias
+        result.name = node[0][0]
+        result.base = node[0][1]
+        return
+      # class A {.pragma.}
+      result.name = node[0]
+    of nnkCommand:
+      if node[1][0].eqIdent"impl":
+        result.kind = Implementation
+        result.name = node[0]
+        if node[1][1].kind == nnkPragmaExpr:
+          # class A impl IA {.pragma.}
+          result.pragmas = node[1][1][1].toSeq()
+          result.base = node[1][1][0]
+          return
+        # class A impl IA
+        result.base = node[1][1]
+        return
+      error "Unsupported syntax", node
+    else:
+      error "Unsupported syntax", node
   of 3:
-    if head.isInheritance:
-      if head[2].kind == nnkPragmaExpr:
-        if "open" in head[2][1]:
-          warning "{.open.} is ignored in a definition of subclass", head
-        return newClassInfo(
-          pragmas = head[2][1].toSeq(),
-          kind = Inheritance,
-          name = head[1],
-          base = head[2][0]
-        )
-      return newClassInfo(
-        kind = Inheritance,
-        name = head[1],
-        base = head[2]
-      )
-    error "Unsupported syntax", head
+    result.isPub = false
+    result.inheritanceClassInfo(head)
   else:
     error "Too many arguments", head
 
