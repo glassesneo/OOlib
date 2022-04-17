f06232a13c1ae4502b40149ee806fbbd2f9c457b
Glasses-Neo <meganeo.programmer@gmail.com>
2022-04-17 14:50:24 +0900

    :sparkles: Support for pragma

--- tests/pragmas.nim
+++ tests/pragmas.nim
@@ -2,6 +2,13 @@ discard """
   action: "compile"
 """
 import ../src/oolib
+import unittest, macros
 
-class A:
-  discard
+template customPragma(v = "") {.pragma.}
+
+class A {.customPragma.}
+
+class B {.customPragma "B".}
+
+check A.hasCustomPragma(customPragma)
+check B.getCustomPragmaVal(customPragma) is NimNode
